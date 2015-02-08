e63eb0e3c7b593a4578c2dcb763556362eb86e10
Lorex <joe50613@gmail.com>
2015-02-08 19:23:30 +0800

    UploadGUI

---
+++ EnvMonitorGUI/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace EnvMonitorGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 47);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "連線";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "建立連線";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(65, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(127, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "序列埠：";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 65);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(279, 184);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "狀態";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "尚未連線"});
+            this.listBox1.Location = new System.Drawing.Point(8, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(265, 148);
+            this.listBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(279, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "黑黑來了，快關燈！";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 289);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Environment Monitor";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EnvMonitorGUI/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnvMonitorGUI
+{
+    public partial class Form1 : Form
+    {
+        bool connected = false;
+        bool activateLED = true;
+        string prevTemp, prevHumid, prevBright;
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] serialPorts = System.IO.Ports.SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(serialPorts);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch(connected)
+            {
+                case true:
+                    if (serialPort1.IsOpen)
+                    {
+                        serialPort1.Close();
+                        listBox1.Items.Clear();
+                        listBox1.Items.Add("尚未連線");
+                        button2.Enabled = false;
+                        timer1.Enabled = false;
+                        button1.Text = "建立連線";
+                        connected = false;
+                    }
+                    break;
+                case false:
+                    try
+                    {
+                        serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                        serialPort1.BaudRate = 115200;
+                    }
+                    catch (Exception ex)
+                    {
+                        listBox1.Items.Add("錯誤：請選擇正確的 COM Port");
+                        break;
+                    }
+                    if (!serialPort1.IsOpen)
+                    {
+                        try
+                        {
+                            serialPort1.Open();
+                            timer1.Enabled = true;
+                            button1.Text = "關閉連線";
+                            button2.Enabled = true;
+                            connected = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            listBox1.Items.Add("錯誤：當前 COM Port 無法建立連線。");
+                            break;
+                        }
+                    }
+                    break;
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            string str = serialPort1.ReadExisting();
+            string[] value = str.Split('/');
+
+
+            string thisTemp = "0", thisHumid = "0", thisBright = "0";
+            bool thisLED = false;
+
+            try
+            {
+                thisTemp = value[0];
+                thisHumid = value[1];
+                thisBright = value[2];
+            }
+            catch (Exception ex)
+            {
+                thisTemp = prevTemp;
+                thisHumid = prevHumid;
+                thisBright = prevBright;
+                thisLED = activateLED;
+            }
+            finally
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("溫度：" + thisTemp + " °C");
+                listBox1.Items.Add("濕度：" + thisHumid + " %");
+                listBox1.Items.Add("環境亮度：" + thisBright + " %");
+                listBox1.Items.Add("照明裝置：" + ((activateLED) ? "開" : "關"));
+
+                prevTemp = thisTemp;
+                prevHumid = thisHumid;
+                prevBright = thisBright;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (activateLED)
+            {
+                if (!serialPort1.IsOpen) return;
+                serialPort1.Write("0");
+            }
+            else
+            {
+                if (!serialPort1.IsOpen) return;
+                serialPort1.Write("1");
+            }
+
+            activateLED = !activateLED;
+            button2.Text = (activateLED) ? "黑黑來了，快關燈！" : "好黑好黑，快開燈！";
+        }
+    }
+}
